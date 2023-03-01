70b9c11c5c3637f221e214a1fcdc4248ab20d029
Étienne Plésiat <92684032+eplesiat@users.noreply.github.com>
2023-03-01 11:18:33 +0100

    Get models info from craimodels repo (#18)
    * Get models info from craimodels repo
    
    * fixed wps tests
    
    * run tests on python 3.10
    
    * pep8
    
    ---------
    
    Co-authored-by: Carsten Ehbrecht <ehbrecht@dkrz.de>

--- duck/clintai.py
+++ duck/clintai.py
@@ -1,72 +1,45 @@
-import os
 from jinja2 import Template
 import shutil
-import collections
 
+import craimodels
 from climatereconstructionai import evaluate
 import logging
 LOGGER = logging.getLogger("PYWPS")
 
-DUCK_HOME = os.path.abspath(os.path.dirname(__file__))
-DATA_DIR = os.path.join(DUCK_HOME, "data")
-
-HADCRUT5_TAS_MEAN = "HadCRUT5"
-HADCRUT4_TEMPERATURE_ANOMALY = "HadCRUT4"
-# HADCRUT4_TAS = "HadCRUT"
-
-HADCRUT = collections.OrderedDict({
-    HADCRUT5_TAS_MEAN: {
-        "variable": "tas_mean",
-        "name": "hadcrut",
-    },
-    HADCRUT4_TEMPERATURE_ANOMALY: {
-        "variable": "temperature_anomaly",
-        "name": "hadcrut",
-    },
-    # HADCRUT4_TAS: {
-    #     "variable": "tas",
-    #     "name": "hadcrut",
-    # },
-})
-
-HADCRUT_VALUES = list(HADCRUT.keys())
+info_models = craimodels.info_models()
+dataset_names = list(info_models.keys())
 
 
-def write_clintai_cfg(base_dir, name, evalname, data_type, dataset_name):
+def write_clintai_cfg(base_dir, model_dir, eval_name, data_name, data_type, eval_parameters):
     cfg_templ = """
     --data-root-dir {{ base_dir }}
     --log-dir {{ base_dir }}
     --mask-dir {{ base_dir }}/outputs
-    --model-dir {{ data_dir }}
-    --model-names 20crtasgn72.pth
+    --model-dir {{ model_dir }}
     --evaluation-dirs {{ base_dir }}/outputs
-    --data-names {{ name }}
+    --eval-names {{ eval_name }}
+    --data-names {{ data_name }}
     --data-types {{ data_type }}
     --maxmem 5000
     --device cpu
-    --n-filters 18
-    --out-channels 1
-    --eval-names {{ evalname }}
     --plot-results 0
-    --global-padding
-    --dataset-name {{ dataset_name }}
+    {{ eval_parameters }}
     """
     cfg = Template(cfg_templ).render(
         base_dir=base_dir,
-        data_dir=DATA_DIR,
-        name=name,
-        evalname=evalname,
+        model_dir=model_dir,
+        eval_name=eval_name,
+        data_name=data_name,
         data_type=data_type,
-        dataset_name=dataset_name)
+        eval_parameters=eval_parameters)
     out = base_dir / "clintai.cfg"
     with open(out, "w") as fp:
         fp.write(cfg)
     return out
 
 
-def run(dataset, hadcrut, outdir, update_status):
-    data_type = HADCRUT[hadcrut]["variable"]
-    dataset_name = HADCRUT[hadcrut]["name"]
+def run(dataset, dataset_name, variable_name, outdir, update_status):
+    # data_type = info_models[dataset_name]["variable-name"]
     (outdir / "masks").mkdir()
     (outdir / "outputs").mkdir()
     input_dir = outdir / "test"
@@ -75,10 +48,11 @@ def run(dataset, hadcrut, outdir, update_status):
     # print(f"dataset={dataset}")
     cfg_file = write_clintai_cfg(
         base_dir=outdir,
-        name=dataset.name,
-        evalname=dataset.stem,
-        data_type=data_type,
-        dataset_name=dataset_name)
+        model_dir=craimodels.model_dir(),
+        eval_name=dataset.stem,
+        data_name=dataset.name,
+        data_type=variable_name,
+        eval_parameters=info_models[dataset_name]["eval-parameters"])
     # print(f"written cfg {cfg_file}")
     try:
         evaluate(arg_file=cfg_file.as_posix(), prog_func=update_status)
